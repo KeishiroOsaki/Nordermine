fb2b9fffa5549b618b3c9507478b988693b9fadb
KeishiroOsaki <startipend2008@gmail.com>
2015-11-30 22:36:00 +0900

    途中まで書いた

--- Nordermine/__main__.py
+++ Nordermine/__main__.py
@@ -4,5 +4,16 @@ Created on 2015/11/30
 @author: osakikeishiro
 '''
 
+import csv
+import sys
+
 if __name__ == '__main__':
     print("おはようございます おはよう世界")
+    args = sys.argv
+    k = int(args[1])
+    inputCSVPath = args[2]
+    outputCSVPath = args[3]
+    useColumns = []
+    for i in range(4,len(args)):
+        useColumns.append(int(args[i]))
+    print(useColumns)
